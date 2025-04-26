e1c661b54b53c60cf8719b10ff9ea60fa5cfb172
ddovwll <robertinkashenoid@gmail.com>
2025-04-26 17:38:06 +0300

    task-three: completed third task

--- ConsoleApps/Tasks/StringReverse.cs
+++ ConsoleApps/Tasks/StringReverse.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApps.Tasks;
 
@@ -7,7 +8,7 @@ public static class StringReverse
     private static readonly Regex LatinLowerCase = new("^[a-z]+$");
     private static readonly Regex InappropriateSymbols = new("[^a-z]");
 
-    public static string Reverse(string input)
+    public static (string reversed, string repetitions) Reverse(string input)
     {
         if (!LatinLowerCase.IsMatch(input))
         {
@@ -16,6 +17,13 @@ public static class StringReverse
             throw new ArgumentException($"Введены неподходящие символы: {inappropriateSymbols}");
         }
 
+        var reversed = ProcessReverse(input);
+        var repetitions = Repetitions(reversed);
+        return (reversed, repetitions);
+    }
+
+    private static string ProcessReverse(string input)
+    {
         if (input.Length % 2 != 0)
         {
             return string.Concat(string.Concat(input.Reverse().ToArray()), input);
@@ -25,5 +33,24 @@ public static class StringReverse
         var secondSubstring = input[(input.Length / 2)..];
         return string.Concat(string.Concat(firstSubstring.Reverse().ToArray()),
             string.Concat(secondSubstring.Reverse().ToArray()));
+    }
+
+    private static string Repetitions(string input)
+    {
+        var charsCount = input
+            .GroupBy(x => x)
+            .ToDictionary(x => x.Key, y => y.Count());
+
+        var stringBuilder = new StringBuilder();
+
+        foreach (var group in charsCount)
+        {
+            stringBuilder.Append(group.Key);
+            stringBuilder.Append(": ");
+            stringBuilder.Append(group.Value);
+            stringBuilder.Append("; ");
+        }
+        
+        return stringBuilder.ToString();
     }
 }
