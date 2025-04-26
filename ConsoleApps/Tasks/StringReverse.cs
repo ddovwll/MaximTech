8abf77321ee1dce3d2506c5390f1347adf54fad8
ddovwll <robertinkashenoid@gmail.com>
2025-04-26 16:01:26 +0300

    task-two: completed second task

--- ConsoleApps/Tasks/StringReverse.cs
+++ ConsoleApps/Tasks/StringReverse.cs
@@ -1,19 +1,26 @@
-﻿namespace ConsoleApps.Tasks;
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApps.Tasks;
 
 public static class StringReverse
 {
+    private static readonly Regex LatinLowerCase = new("^[a-z]+$");
+    private static readonly Regex InappropriateSymbols = new("[^a-z]");
+
     public static string Reverse(string input)
     {
-        if (string.IsNullOrWhiteSpace(input))
+        if (!LatinLowerCase.IsMatch(input))
         {
-            return string.Empty;
+            var matches = InappropriateSymbols.Matches(input);
+            var inappropriateSymbols = string.Join(", ", matches.Select(m => m.Value).ToHashSet());
+            throw new ArgumentException($"Введены неподходящие символы: {inappropriateSymbols}");
         }
 
         if (input.Length % 2 != 0)
         {
             return string.Concat(string.Concat(input.Reverse().ToArray()), input);
         }
-        
+
         var firstSubstring = input[..(input.Length / 2)];
         var secondSubstring = input[(input.Length / 2)..];
         return string.Concat(string.Concat(firstSubstring.Reverse().ToArray()),
