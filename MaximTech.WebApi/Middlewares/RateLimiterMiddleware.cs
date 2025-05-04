d40eb82eda8d193e25fd5744917d135d641d3988
ddovwll <robertinkashenoid@gmail.com>
2025-05-04 17:33:54 +0300

    task-nine: completed ninth task

---
+++ MaximTech.WebApi/Middlewares/RateLimiterMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using MaximTech.Domain.Models;
+
+namespace MaximTech.WebApi.Middlewares;
+
+public class RateLimiterMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly SemaphoreSlim _semaphore;
+
+    public RateLimiterMiddleware(RequestDelegate next, Settings settings)
+    {
+        _next = next;
+        _semaphore = new SemaphoreSlim(settings.ParallelLimit, settings.ParallelLimit);
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var acquired = await _semaphore.WaitAsync(0);
+
+        if (!acquired)
+        {
+            context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+            return;
+        }
+
+        try
+        {
+            await _next(context);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
